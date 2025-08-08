3eb47e126dd8d3feb3d8def90aa4f5126fc78150
Sanskruti Lonkar <your_github_email@example.com>
2025-08-08 14:13:24 +0530

    Initial commit

---
+++ aahaar-app-1/src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+function Home() {
+  return (
+    <div>
+      <header>Aahaar</header>
+      <nav>
+        <p>Food Donation and Redistribution Platform</p>
+        <a href='Home'>Home</a>
+        <a href='About'>About</a>
+        <a href='Contact'>Contact</a>
+        <a href='SignUp'>SignUp</a>
+        <a href='Sign in'>Sign in</a>
+      </nav>
+      
+     <img src="/food2.jpeg" alt="food" width={850} height={850} />
+
+
+
+     
+      <footer>
+        Donate some food@2005
+      </footer>
+    </div>
+    
+   
+  )
+}
+
+export default  Home;
